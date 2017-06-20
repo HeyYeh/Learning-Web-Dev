98c7e36e6835fb4362088ef74508eda7aca6453f
HeyYeh <ian-bousher@hotmail.com>
2017-06-20 20:14:46 +0100

    Slight progress with the numbers
    Need to fix generation when there are dulicates

--- guts.js
+++ guts.js
@@ -1,4 +1,28 @@
-function Print() {
-    "use strict";
-    document.getElementById('gogo').innerHTML = Math.floor(Math.random() * 59);
+var winners = [];
+
+function Print() 
+{  
+  
+  for (i = 0; i < 6; i++)
+    {
+      winners[i] = Math.floor(Math.random() * 59);
+      
+      winners = winners.filter( function( item, index, inputArray ) {
+           return inputArray.indexOf(item) == index;
+    });
+    }
+  
+    winners.sort( function(a,b) { return a - b; } );
+    
+
+    
+      for (i = 0; i < 6; i++) 
+      {
+        document.getElementById("CompLotto" + String([i])).innerHTML = winners[i];
+      }
+    
 }
+  
+
+
+  document.getElementById('goo').innerHTML = winners;
