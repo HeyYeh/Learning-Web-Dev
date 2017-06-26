7c5ef231cef36a12cd3cf2a294f322be696c1c6a
HeyYeh <ian-bousher@hotmail.com>
2017-06-26 22:28:35 +0100

    Everything is broken
    And I don't know why

--- guts.js
+++ guts.js
@@ -3,48 +3,57 @@ var winners = [];
 
 //function for generating winning numbers (random, all unique, ordered)
 function Print() {
-//testing if your numbers are okay to play with
+    //testing if your numbers are okay to play with
     "use strict";
     var message, x;
     message = document.getElementById("goo");
-    message.innerHTML = "";
+    message.innerHTML = " ";
     x = document.getElementById("puck").value;
-    try { 
-        if(x == "") throw "is Empty";
-        if(isNaN(x)) throw "not a number";
-        if(x > 59) throw "too high";
-        if(x < 1) throw "too low";
-    }
-    catch(err) {
+    try {
+        if (x === " ") {
+            throw "is Empty";
+        }
+        if (isNaN(x)) {
+            throw "not a number";
+        }
+        if (x > 59) {
+            throw "too high";
+        }
+        if (x < 1) {
+            throw "too low";
+        }
+     catch (err) {
         message.innerHTML = "Input " + err;
     }
-}
-  
-  
+    }
+
+
+
     for (i = 0; i < 6; i++) {
         winners[i] = Math.floor(Math.random() * 59);
 
-        winners = winners.filter(function(item, index, inputArray) {
+        winners = winners.filter(function (item, index, inputArray) {
             return inputArray.indexOf(item) == index;
         });
     }
     while (winners.length < 6) {
-        winners.push(Math.floor(Math.random() * 59))
+        winners.push(Math.floor(Math.random() * 59));
 
-        winners = winners.filter(function(item, index, inputArray) {
-            return inputArray.indexOf(item) == index;
+        winners = winners.filter(function (item, index, inputArray) {
+            return inputArray.indexOf(item) === index;
         });
     }
     //sorting winners
-    winners.sort(function(a, b) {
+    winners.sort(function (a, b) {
         return a - b;
     });
     //writing winners to html
     for (i = 0; i < 6; i++) {
         document.getElementById("CompLotto" + String([i])).innerHTML = winners[i];
     }
-    //testing
-    //document.getElementById('goo').innerHTML = winners;
+}
+//testing
+//document.getElementById('goo').innerHTML = winners;
 
 
 
@@ -53,13 +62,12 @@ function Error() {
     message = document.getElementById("goo");
     message.innerHTML = "";
     x = document.getElementById("puck").value;
-    try { 
-        if(x == "") throw "is Empty";
-        if(isNaN(x)) throw "not a number";
-        if(x > 59) throw "too high";
-        if(x < 0) throw "too low";
-    }
-    catch(err) {
+    try {
+        if (x === "") throw "is Empty";
+        if (isNaN(x)) throw "not a number";
+        if (x > 59) throw "too high";
+        if (x < 0) throw "too low";
+    } catch (err) {
         message.innerHTML = "Input " + err;
     }
 }
